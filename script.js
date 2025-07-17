fa18a357f214acaf1d6ef959b35c48535f5af508
OMR-Trend <nitheesgopi@gmail.com>
2025-07-17 14:12:09 +0530

    updated

--- script.js
+++ script.js
@@ -1,8 +1,19 @@
-console.log('hello')
+
+
+console.log('hello');
 
 //git commands 
 
+//To push the code
 //git init - 
 //git config --global user.email "xyz@gmail.com"
 //git config --global user.name "vj-trend"
 //git add . => staging area
+//git commit -m "message"
+//git remote add origin https://github.com/VJ-trend/July_IPT_25.git
+//git branch -M main 
+//git push -u origin main 
+
+
+
+console.log("newly added")
